c7167312f1f5aace7f42c3b9aca5e135c5b621ed
Renan Bruce <bruce.renan@hotmail.com>
2020-11-13 00:20:00 -0400

    Save Tweets, Pagination

--- app/Http/Livewire/ShowTweets.php
+++ app/Http/Livewire/ShowTweets.php
@@ -2,16 +2,35 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Tweet;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class ShowTweets extends Component
 {
-    public $message = 'Apenas um teste';
+    use WithPagination;
+
+    public $content = 'Apenas um teste';
+
+    protected $rules = [
+        'content' => 'required|min:3|max:255'
+    ];
 
     public function render()
     {
-        $tweets = \App\Models\Tweet::with('user')->get();
+        $tweets = \App\Models\Tweet::with('user')->paginate(2);
         return view('livewire.show-tweets', compact('tweets'));
+    }
+
+    public function create()
+    {
+        $this->validate();
+        Tweet::create([
+            'content' => $this->content,
+            'user_id' => 1
+        ]);
+
+        $this->content = '';
     }
        
 }
